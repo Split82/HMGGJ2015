42335b68183edd937c26f8db0b4f38d08db132fe
Lokiman <loki@lokiman.com>
2015-01-24 00:24:01 +0100

    pixelart camera

--- GGJ2015/Assets/Scripts/PixelArtCamera.cs
+++ GGJ2015/Assets/Scripts/PixelArtCamera.cs
@@ -4,14 +4,15 @@ using System.Collections;
 public class PixelArtCamera : MonoBehaviour {
 
 	public int _pixelMultiplier = 2;
+	public int _pixelsPerUnit = 48;
 
 	void Start () {
 	
-		//Camera selfCamera = gameObject.GetComponent<Camera> ();
 	}
 	
 	void Update () {
 
-
+		Camera selfCamera = gameObject.GetComponent<Camera> ();
+		selfCamera.fieldOfView = Mathf.Atan (((float)Screen.height / (2 * _pixelsPerUnit * _pixelMultiplier)) / -gameObject.transform.position.z) * Mathf.Rad2Deg;
 	}
 }
