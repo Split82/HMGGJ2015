a7cb532a8ff5bb653a188f0a2fbcedf4f5899ccd
Lokiman <loki@lokiman.com>
2015-01-23 23:39:43 +0100

    daco pre sticka

---
+++ GGJ2015/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public char[,] GenerateLevel() {
+
+		char[,] data = new char[10, 10];
+
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {			
+				data[i, j] = ' ';
+			}
+		}
+
+		return data;
+	}
+}
---
+++ GGJ2015/Assets/Scripts/PixelArtCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PixelArtCamera : MonoBehaviour {
+
+	public int _pixelMultiplier = 2;
+
+	void Start () {
+	
+		//Camera selfCamera = gameObject.GetComponent<Camera> ();
+	}
+	
+	void Update () {
+
+
+	}
+}
