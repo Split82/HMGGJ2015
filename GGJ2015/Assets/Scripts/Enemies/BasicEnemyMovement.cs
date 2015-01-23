8244e17dbc7187d300dcf7f75a65ec70e781b36f
Lokiman <loki@lokiman.com>
2015-01-23 21:42:46 +0100

    basic enemy movement

---
+++ GGJ2015/Assets/Scripts/Enemies/BasicEnemyMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyMovement : MonoBehaviour {
+
+	public float _speed = 1.0f;
+
+	private Rigidbody2D _rigidbody2D;
+
+	void Awake() {
+
+		_rigidbody2D = rigidbody2D;
+	}
+
+	void Start () {
+	}
+}
