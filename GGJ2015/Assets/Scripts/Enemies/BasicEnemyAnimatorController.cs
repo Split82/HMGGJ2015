40ccf398f0b66d73d97afa6196ca8ed24b467503
Jan Ilavsky <Split@Barboras-MacBook.local>
2015-01-24 18:07:48 +0100

    BasicEnemyAnimation

---
+++ GGJ2015/Assets/Scripts/Enemies/BasicEnemyAnimatorController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BasicEnemyAnimatorController : MonoBehaviour {
+
+	public Animator _animator;
+	public FaceDirection _faceDirection;
+	
+	private int _leftAnimatorParam;
+	
+	void Start () {
+		
+		Check.Null(_animator);
+		Check.Null(_faceDirection);
+
+		_leftAnimatorParam = Animator.StringToHash("Left");
+	}
+	
+	void Update () {
+
+		_animator.SetBool(_leftAnimatorParam, _faceDirection.Direction == FaceDirection.DirectionEnum.Left);
+	}
+}
