2219574b09c0e4bf6bc06d9f6c2afa0fa2d6b94e
Jan Ilavsky <Split@Barboras-MacBook.local>
2015-01-24 03:18:06 +0100

    BG

---
+++ GGJ2015/Assets/Scripts/Helpers/FixedFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedFollow: MonoBehaviour {
+
+	public GameObject followObject;
+	public Vector3 offset;
+	
+	private Transform followTransform;
+	private Transform thisTransform;
+	
+	void Start () {
+
+		thisTransform = this.transform;
+		followTransform = followObject.transform;
+		thisTransform.position = followTransform.position + offset;	
+	}
+	
+	void Update () {
+
+		thisTransform.position = followTransform.position + offset;	
+	}
+}
