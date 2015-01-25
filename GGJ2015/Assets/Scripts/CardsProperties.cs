a1dd1a9753c393a4cadd0955212883efd300ae55
Peter Hrincar <peter.hrincar@gmail.com>
2015-01-25 08:07:28 +0100

    card manager

---
+++ GGJ2015/Assets/Scripts/CardsProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardsProperties : MonoBehaviour {
+
+	[System.Serializable]
+	public class CardProperties {
+		public string id;
+
+		public Sprite icon1;
+		public Sprite icon2;
+
+		public string text1;
+		public string text2;
+	}
+	public CardProperties[] cardProperties;
+}
