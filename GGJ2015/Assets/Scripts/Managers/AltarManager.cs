9000699661959c3caaec26aa08e20213a6f3aecb
Jan Ilavsky <Split@Barboras-MacBook.local>
2015-01-25 03:51:39 +0100

    Bird

---
+++ GGJ2015/Assets/Scripts/Managers/AltarManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AltarManager : Singleton<AltarManager> {
+
+	private List<Altar> _altars;
+
+	public Altar AltarWithCard {
+		get {
+			return _altarWithCard;
+		}
+	}
+
+	private Altar _altarWithCard;
+
+	void Awake() {
+		_altars = new List<Altar>();
+	}
+
+	void Start() {
+
+		SpawnCardOnRandomAltar();
+	}
+	
+	public void RegisterAltar(Altar altar) {
+
+		_altars.Add(altar);
+	}
+
+	public void SpawnCardOnRandomAltar() {
+
+		_altarWithCard = _altars[Random.Range(0, _altars.Count)];
+		_altarWithCard.HasCard = true;
+	}
+
+}
