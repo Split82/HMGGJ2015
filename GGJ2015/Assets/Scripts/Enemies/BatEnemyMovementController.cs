efcc429f1a7a945c3941367902fcff3f3a1298c7
Peter Hrincar <peter.hrincar@gmail.com>
2015-01-25 03:43:37 +0100

    bat movement controller

---
+++ GGJ2015/Assets/Scripts/Enemies/BatEnemyMovementController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody2D))]
+
+public class BatEnemyMovementController : MonoBehaviour {
+
+	public BatEnemyMovement _batEnemyMovement;
+
+	public float _checkInterval = 0.1f;
+	public float _attackDistance = 50f;
+
+	private Rigidbody2D _rigidBody2D;
+
+	void Start() {
+
+		Check.Null(_batEnemyMovement);
+		_rigidBody2D = GetComponent<Rigidbody2D>();
+		StartCoroutine(ChangeState());
+		StartCoroutine(AttackPositionUpdate());
+	}
+
+	IEnumerator AttackPositionUpdate() {
+		
+		while (true) {
+			
+			if (_batEnemyMovement.State == BatEnemyMovement.BatEnemyStateEnum.Follow) {
+				_batEnemyMovement.AttackLocation = GameplayManager.Instance._playerController._playerTransform.position;
+			}
+			yield return new WaitForSeconds(_checkInterval);
+		}
+	}
+
+	IEnumerator ChangeState() {
+
+		while (true) {
+			Vector2 direction = (_batEnemyMovement.AttackLocation - new Vector2(_rigidBody2D.transform.position.x, _rigidBody2D.transform.position.y));		                   
+
+			if (_batEnemyMovement.State == BatEnemyMovement.BatEnemyStateEnum.Follow && direction.magnitude < _attackDistance) {
+				_batEnemyMovement.State = BatEnemyMovement.BatEnemyStateEnum.Attack;
+			}
+			else if (_batEnemyMovement.State == BatEnemyMovement.BatEnemyStateEnum.Attack && direction.magnitude < 0.1) {
+				_batEnemyMovement.State = BatEnemyMovement.BatEnemyStateEnum.Follow;
+			}
+			
+			yield return new WaitForSeconds(_checkInterval);
+		}
+	}
+
+}
